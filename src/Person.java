715c132179e7b79d8520a23923a12b8943e78d3d
emeraldemris <davidallen0550@gmail.com>
2020-03-31 16:03:38 -0700

    Fixed the code before adding a menu and interaction
    Fixed the code before adding a menu and interaction

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,15 @@
 public class Person {
-
     private String name;
     private String title;
-    private Icecream icecream;
+    private IceCream icecream;
 
     public Person(final String name, final String title) {
-
         this.name = name;
         this.title = title;
+        int x = Math.min(1, 2);
     }
 
+<<<<<<< Updated upstream
         public Object getName () {
             return this.name;
         }
@@ -18,13 +18,36 @@ public class Person {
             return this.title;
             //david
         }
+=======
+    public Object getName() {
+        return this.name;
+    }
+>>>>>>> Stashed changes
+
+    public String getTitle() {
+        return this.title;
+    }
 
 
-
-    public String getFullTitle () {
+    public String getFullTitle() {
         return this.getName() + " " + this.getTitle();
     }
-    public void setIcecream(final Icecream icecream) {this.icecream = icecream;
+
+    public void setIcecream(final IceCream icecream) {
+        this.icecream = icecream;
     }
 
+    public String getIceCreamType() {
+        //turinary operator : equvilent to "if icecream is null, then nothing, else icecream type"
+        return this.icecream == null ? "nothing": this.icecream.getIceCreamType();
+        //return this.icecream.getIceCreamType();
+    }
+
+    public int getCalories() {
+        return this.icecream.getFoodCalories();
+    }
+
+    public void printData() {
+        System.out.println(this.getCalories());
+    }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
-public class Solution {
 
+<<<<<<< Updated upstream
     public static void main(final String[] args) {
         System.out.println("what kind of IceCream is David eating");
 
@@ -7,17 +7,52 @@ public class Solution {
         final Person noah = new Person( "Noah", "Buffalo Tyrant");
         Person zach = new Person("Zach", "of Seatac");
         zach = new Person("Zach", "of Seatac 2");
+=======
 
-        final Icecream chocolate = new Icecream( "chocolate", 100);
+public class Solution {
+    //this variable is static which means it's universal. There is one program name in the whole program.
+private static final String programName = "David's Program";
+private static String status = "Starting"; //because there is only one status but it could be updated
+//final each icecream has it's own type but it's type can't change
+    public static void main(String[] args) {
+
+
+        System.out.println("what kind of IceCream is David eating");
+
+        final Person david = new Person("David", "Cheese Lord");
+        final Person noah = new Person( "Noah", "Buffalo King");
+        final Person zach = new Person("Zach", "of Seatac");
+
+        //new calls the constructor for icecream
+        //local varriable (only exists in main)
+        final IceCream peanutbutter = new IceCream( "peanutbutter", 150000);
+        final IceCream chocolate = new IceCream( "chocolate", 100000);
+>>>>>>> Stashed changes
+
 
         System.out.println("unsure test");
 
-        david.setIcecream(chocolate);
+        zach.setIcecream(chocolate);
+        david.setIcecream(peanutbutter);
+        noah.setIcecream(null);
+
 
 	//david
 
+<<<<<<< Updated upstream
         System.out.println(david.getFullTitle() +  " is eating " + david.getIceCreamType);
         System.out.println(zach.getFullTitle() + " tbd");
         System.out.println(noah.getFullTitle() + " tbd");
+=======
+        System.out.println(david.getFullTitle() +  " is eating " + david.getIceCreamType());
+        System.out.println(zach.getFullTitle() + "is eating " + zach.getIceCreamType());
+        System.out.println(noah.getFullTitle() + " is eating " + noah.getIceCreamType());
+
+        david.printData();
+
+
+
+
+>>>>>>> Stashed changes
     }
 }
